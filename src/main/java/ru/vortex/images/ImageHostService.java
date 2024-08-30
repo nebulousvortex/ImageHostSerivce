47dd52f17a0946744c77c856d3d4b7b085837193
Слобожанин Федор <fslob@getcrm.ru>
2024-08-30 15:41:01 +0300

    Initial Commit

---
+++ src/main/java/ru/vortex/images/ImageHostService.java
@@ -0,0 +1,11 @@
+package ru.vortex.images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageHostService {
+    public static void main(String[] args) {
+        SpringApplication.run(ImageHostService.class, args);
+    }
+}
---
+++ src/main/java/ru/vortex/images/controller/ImageController.java
@@ -0,0 +1,36 @@
+package ru.vortex.images.controller;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController()
+@CrossOrigin(origins = {"http://localhost:3000" , "https://leafcity.vercel.app", "https://leafcity.ru"})
+@RequestMapping("/fs")
+public class ImageController {
+
+    @GetMapping("/images/{path}/{imageName}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String imageName, @PathVariable String path) throws IOException {
+
+        var imgFile = new ClassPathResource("images/" + path +'/'+ imageName + ".webp");
+        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(bytes);
+    }
+    @GetMapping("/images/{path1}/{path2}/{imageName}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String imageName, @PathVariable String path1, @PathVariable String path2) throws IOException {
+
+        var imgFile = new ClassPathResource("images/" + path1 +'/'+ path2 +'/'+ imageName + ".webp");
+        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(bytes);
+    }
+}
